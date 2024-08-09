c3df7cfd062503eec27b8cd29e047885352bc99d
the-man2004 <heinrichhb111@gmail.com>
2024-08-09 11:24:13 -0800

    Added & styled the about page content.

--- src/app/routes/about/about.component.ts
+++ src/app/routes/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../../shared/logo/logo.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './about.component.html',
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {}
