918833404921d7919759f7594839f46303baacb1
the-man2004 <heinrichhb111@gmail.com>
2024-08-09 16:16:21 -0800

    Added & styled the content of the team component.

--- src/app/routes/team/team.component.ts
+++ src/app/routes/team/team.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../../shared/logo/logo.component';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './team.component.html',
+  styleUrl: './team.component.css',
 })
 export class TeamComponent {}
