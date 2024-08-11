78bbb61f8c1715c087e5f911f7917e5c2d63c287
the-man2004 <heinrichhb111@gmail.com>
2024-08-11 12:32:42 -0800

    Added & styled content for the solution areas component.

--- src/app/routes/solution-areas/solution-areas.component.ts
+++ src/app/routes/solution-areas/solution-areas.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../../shared/logo/logo.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-solution-areas',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent, RouterModule],
   templateUrl: './solution-areas.component.html',
 })
 export class SolutionAreasComponent {}
