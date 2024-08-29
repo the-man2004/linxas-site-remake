a7ef0641a094a3e795b929cdeaefb4962eff937e
the-man2004 <heinrichhb111@gmail.com>
2024-08-28 16:18:39 -0800

    Added the footer along with all of it's content & styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './routes/contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, ContactComponent],
+  imports: [RouterModule, HeaderComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
